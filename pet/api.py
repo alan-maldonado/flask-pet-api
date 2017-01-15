bdb752fd07276c3ea02a560b66cdeb737c17c227
alan-maldonado <alan.maldonado@wizeline.com>
2017-01-15 15:01:47 -0600

    add first get request

---
+++ pet/api.py
@@ -0,0 +1,12 @@
+from flask.views import MethodView
+from flask import jsonify, request
+
+class PetAPI(MethodView):
+    pets = [
+        { "id": 1, "name": u"Mac" },
+        { "id": 2, "name": u"Leo" },
+        { "id": 3, "name": u"Brownie" }
+    ]
+
+    def get(self):
+        return jsonify({"pets": self.pets})
